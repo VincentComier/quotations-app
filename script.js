2b02d93b395ff4a205ee798887e6f29605c62156
VincentComier <V.Comier@hotmail.fr>
2025-07-04 10:48:08 +0200

    etape 4

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form")
+const text = document.getElementById("citations")
+const author = document.getElementById("auteur/rice")
+
+console.log("c'est l'auteur:" , author)
+console.log("c'est le text:" ,text)
+
+form.addEventListener("click", () => {
